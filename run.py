6a6a88ab6bb492ebef34cc32ad69904fcb953950
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-01 01:50:06 +0100

    Added function to pull word from the words folder

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 import random
 from words import words
 
+def find_valid_words(words):
+    """
+This function pulls a random word from the words.py folder, checks
+if it has a - or a space in between the word then picks another random word if 
+it has one of these. It would could become confusing for the player.
+    """
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word
