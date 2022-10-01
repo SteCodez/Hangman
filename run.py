c1bcf08e455376f4ce1f2b49d08825fb95bd56fd
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-01 02:17:38 +0100

    Adding notes to clarify functions

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ This function pulls a random word from the words.py folder, checks
 if it has a - or a space in between the word then picks another random word if 
 it has one of these. It would could become confusing for the player.
     """
-    word = random.choice(words)
-    while '-' in word or ' ' in word:
+    word = random.choice(words) #pulls a random word from the words.py folder.
+    while '-' in word or ' ' in word: #Makes the gam find a work not seperated by ' ' space or '-'
         word = random.choice(words)
     return word
 
@@ -85,7 +85,11 @@ def get_hangman_stages(remaining_attempts):
     return stages[max_attempts - remaining_attempts]
 
 def print_hidden_words(hidden_word):
-    print("_" * len(hidden_word))
+    """
+    This function pulls the words from the find_valid_words funtion and displays them as a _
+    which creates the main function of the hangman game.
+    """
+    print("_" * len(hidden_word)) #changes the words to underscores
 
 print("Welcome! Hope you enjoy this game of hangman :) \n")
 hidden_word = find_valid_words(words)
