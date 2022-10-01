fd2a72f409c8c0374d69f7607ae280dd0c116279
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-01 20:02:23 +0100

    Called get stages funtion from stages.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
-from hangman_stages import get_hangman_stages
+import hangman_stages
 from words import words
+remaining_attempts = 8
 
 def find_valid_words(words):
     """
@@ -20,11 +21,9 @@ def print_hidden_words(hidden_word):
     This function pulls the words from the find_valid_words funtion and displays them as a _
     which creates the main function of the hangman game.
     """
-    print("_" * len(hidden_word)) #changes the words to underscores
+    print(" _ " * len(hidden_word)) #changes the words to underscores
 
-    print("Welcome! Hope you enjoy this game of hangman :) \n")
-    hidden_word = find_valid_words(words)
-    print_hidden_words(hidden_word)
-
+print("Welcome! Hope you enjoy this game of hangman :) \n")
+hidden_word = find_valid_words(words)
 print(hangman_stages.get_hangman_stages(remaining_attempts))
-print(print_hidden_words)
+print_hidden_words(hidden_word)
